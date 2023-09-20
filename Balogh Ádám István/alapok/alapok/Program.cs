7a2400533f462b07a5205dd64dd07a577149cb88
Adam Balogh <adam.balogh@katonaj-mkszig.hu>
2023-09-20 09:39:14 +0200

    fonal

---
+++ Balogh Ádám István/alapok/alapok/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alapok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hello világ!");
+            //Console.Beep(120, 200);
+            //int be = Console.Read();
+            //string be2 = Console.ReadLine();
+
+            //Console.WriteLine(be);
+            //int x = 22;
+            //double y = (double)x;
+            //Console.WriteLine(y);
+            //int x = 3;
+            //Console.WriteLine((double);
+            /*byte b = 11;
+            Console.WriteLine(b);
+            int egesz = b;
+            Console.WriteLine(egesz);
+            double d = egesz;
+            Console.WriteLine(d);*/
+            Console.WriteLine("add meg a négyzet oldalát");
+            int a =Convert.ToInt32(Console.ReadLine)();
+            int kerulet = 4 * a;
+            int kerulet = a * a;
+            Console.WriteLine("A négyzet kerülete" + kerulet);
+            Console.WriteLine("A négyzet területe" + terulet);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
