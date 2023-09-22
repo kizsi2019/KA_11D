6c55948e81df530ed9ce7d655a461c1aad0012ca
Levi <levjuce2004@gmail.com>
2023-09-22 10:01:12 +0200

    kkk

---
+++ Vicsápi Levente/alapok/alapok/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alapok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Beep();
+            Console.Beep(9999, 9999);
+            */
+
+            /*int be = Console.Read();
+            string be2 = Console.ReadLine();
+            Console.WriteLine(be);*/
+            /*
+            int x = 22;
+            double y = (double)x;
+            Console.WriteLine(y);
+            */
+
+            /*
+            int x = 3;
+            Console.WriteLine(x / 4);
+
+            Console.WriteLine((double)x / 4);
+            */
+
+            /*
+            byte b = 30;
+            int egesz = b;
+            double d = egesz;
+
+            Console.WriteLine(b);
+            Console.WriteLine(egesz);
+            Console.WriteLine(d);
+            */
+
+            /*
+            Console.Write("Add meg az oldalát! ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("A négyzet kerülete: " + Kerület(a));
+            Console.WriteLine("A négyzet területe: " + Terület());
+
+            Console.ReadKey();
+
+            
+
+            static int Kerület(int a)
+            {
+                int kerület = 4 * a;
+
+                return kerület;
+            }
+
+            static int Terület(int a)
+            {
+                int terület = a * a;
+
+                return terület;
+            }
+
+            */
+
+            Console.Write("Add meg a kör sugarát! ");
+            int sugar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A kör kerülete " + Kerület(sugar);
+            Console.WriteLine("A kör területe " + Terület(sugar);
+
+
+
+            static double Terület(int oldal)
+
+            {
+                double terület = sugar * sugar * 3.14;
+
+                return terület;
+            }
+
+            static double Kerület(int oldal)
+            {
+                double kerület = 2 * sugar * 3.14;
+
+                return kerület;
+            }
+        }
+
+        
+         
+        
+    }
+}
