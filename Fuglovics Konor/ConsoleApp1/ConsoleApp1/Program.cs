823c4cccd41af2806601fc118d6251f9e9193a6d
SoundCrafterYT <konor.fuglovics@gmail.com>
2023-09-27 14:24:04 +0200

    XAMPP SQL

---
+++ Fuglovics Konor/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Math.Abs(-3);
+            Console.WriteLine(a);
+            double b = Math.Ceiling(1.23);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
