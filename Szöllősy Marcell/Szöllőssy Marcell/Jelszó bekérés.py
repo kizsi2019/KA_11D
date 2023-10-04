a88c37089ce82beb8adffceac3aa5269c188ba98
Jakab Mate Tibor <tibormate.jakab@katonaj-mkszig.hu>
2023-10-04 13:02:33 +0200

    mai

--- Szöllősy Marcell/Szöllőssy Marcell/Jelszó bekérés.py
+++
@@ -1,9 +0,0 @@
-valasz = input("Adja meg a jelszót!")
-jelszó = "kutyahát"
-folytatja = True
-while folytatja:
-    if not valasz == jelszó:
-        valasz = input("Add meg a jelszót)")
-    else:
-        folytatja = False
-print("Eltaláltad a jelszót!")
