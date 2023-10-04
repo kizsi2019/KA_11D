812b9eeb6160cf8e31210727c8a3fcd2892a4773
payloaddrone <zsomborkissfodor@gmail.com>
2023-10-04 08:53:04 +0200

    felszekszelem

---
+++ Kiss-Fodor Zsombor/dot net/New folder/Case cuccok/Case cuccok/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case_cuccok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Menü program");
+            Console.WriteLine("Opciók:");
+            Console.WriteLine("A: Kis kávé, B: Nagy kávé");
+            Console.WriteLine("C: Nem iszom semmit");
+            Console.Write("Kérem válasszon!: ");
+            var gomb = (char)Console.Read();
+
+            switch (gomb)
+            {
+                case 'A':
+                case 'a':
+                    Console.WriteLine("Kis kávét választott");
+                    break;
+                case 'B':
+                case 'b':
+                    Console.WriteLine("Nagy kávét választott");
+                    break;
+                case 'C':
+                case 'c':
+                    Console.WriteLine("KURVA ANYÁDAT THE FASZ");
+                    break;
+                default:
+                    Console.WriteLine("Nem jó gombot nyomott!");
+                    break;
+            }
+            
+            try
+            {
+                string szoveg = "valami szöveg";
+                int szam = Convert.ToInt32(szoveg);
+                Console.WriteLine(szam);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Valami hiba történt: {0}", ex.Message);
+            }
+
+
+
+            Console.WriteLine("Kettővel szorzó v. 1.0");
+            Console.WriteLine("Adjon meg egy egész páros számot!");
+            try
+            {
+                var bevitel = Console.ReadLine();
+                int szam = Convert.ToInt32(bevitel);
+
+                if ((szam % 2) != 0)
+                {
+                    throw new Exception("A szám nem páros");
+                }
+                Console.WriteLine($"A szorzás eredménye: {szam * 2}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HIBA történt");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program vége. Nyomjon egy gombot a kilépéshez");
+                Console.ReadKey();
+            }
+            
+            int szam;
+            if (int.TryParse("123", out szam))
+            {
+                //sikeres volt a feldolgozás
+                //ebben a blokkban a szam változó
+                //biztos, hogy helytálló értékkel rendelkezik.
+            }      
+
+            */
+            Console.WriteLine("kérek egy számocskát te csóró!!");
+            try
+            {
+                var bevitel = Console.ReadLine();
+                int szam = Convert.ToInt32(bevitel);
+
+                if (int.TryParse(bevitel, out szam))
+                {
+                    int kerulet = 6 * szam;
+                    Console.WriteLine("a hatszög területe " + kerulet);
+                }
+                else
+                {
+                    throw new Exception("A szám nem páros");
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HIBA történt");
+                Console.WriteLine(ex.Message);
+            }
+            
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
