dd58f141436e3238a6c392c1960c45ed097bfbf8
SoundCrafterYT <konor.fuglovics@gmail.com>
2023-10-13 12:32:42 +0200

    score LOTR

--- Fuglovics Konor/kocka/js/app.js
+++ Fuglovics Konor/kocka/js/app.js
@@ -17,6 +17,10 @@ ActivePlayer = 1;
 //document.querySelector('#current-' + ActivePlayer).textContent = block;
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = "0";
+document.getElementById('current-0').textContent = "0";
+document.getElementById('score-1').textContent = "0";
+document.getElementById('current-1').textContent = "0";
 
 document.querySelector('.btn-roll').addEventListener("click", function(){
   var block = Math.floor(Math.random() * 6) + 1;
