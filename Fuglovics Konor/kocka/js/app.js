6ee1b2579bd8a53baf66c04cd5fe9759745a9b05
SoundCrafterYT <konor.fuglovics@gmail.com>
2023-10-13 10:50:40 +0200

    s

--- Fuglovics Konor/kocka/js/app.js
+++ Fuglovics Konor/kocka/js/app.js
@@ -1,5 +1,4 @@
-/*
-Játék szabályok:
+/*Játék szabályok:
 
 - A játék 2 szereplős és körökre osztott
 - Minden egyes körben az adott játékos dob a kockával, ahányszor csak szeretne. A dobások eredménye hozzáadódik a játékos adott körben
@@ -7,6 +6,5 @@ Játék szabályok:
 - Ha az aktuális játékos 1-et dob, akkor az összes addigi pontja elveszik, és átadja a dobás jogát a következő játékosnak.
 - A játékos választhatja a 'Megtartom' gombot is. Ebben az esetben az adott körben elért pontok száma, hozzáadódik a játékos összes
   pontszámához. Majd a dobás joga a másik játékosra száll.
-- Az a játékos nyer, aki előbb eléri a 100 pontot.  
+- Az a játékos nyer, aki előbb eléri a 100 pontot.*/
 
-*/
