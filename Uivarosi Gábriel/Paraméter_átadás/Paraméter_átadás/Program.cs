ed5162dd247ef8f3611be1af59972d43960c1101
Gabi455 <90388665+Gabi4555@users.noreply.github.com>
2023-10-18 13:13:15 +0200

    l

---
+++ Uivarosi Gábriel/Paraméter_átadás/Paraméter_átadás/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paraméter_átadás
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         List<int> Régilista = new List<int>() { 20, 31, 10};
+            List<int> újralista = Régilista;
+            újralista.Add(40);
+            Console.WriteLine("Régilista Tartalma");
+              foreach (int i in Régilista) Console.WriteLine(i);
+            Console.WriteLine("Újgilista Tartalma");
+            foreach (int i in újralista) Console.WriteLine(i);
+
+
+            Console.ReadKey();
+            Random random = new Random();
+            while (true)
+            {
+                int h = random.Next(1, 59);
+                int w = random.Next(1, 59);
+                Console.SetWindowSize(w, h);
+            }
+        }
+    }
+}
