56cb9c7a6663e3650f90195ac194700c8b67d9e3
Adam Balogh <adam.balogh@katonaj-mkszig.hu>
2023-10-18 09:31:10 +0200

    nesze2

--- Balogh Ádám István/csharp/összetett/összetett/Program.cs
+++ Balogh Ádám István/csharp/összetett/összetett/Program.cs
@@ -59,7 +59,27 @@ namespace összetett
            List<int> lista = new List<int>() { 10 ,23, 4, 30, 10, 16, 19, 20, 34, 10};
             lista.Clear();
             Console.WriteLine("Törlés után a kapacitása: {0}, mérete:{1}", lista.Capacity,);
+            
+           List<string> lista = new List<string>() { "banán", "alma", "körte", "narancs", "kiwi"};
+            lista.Sort();
+            foreach (string item in lista)
+            {
+                Console.WriteLine(item);
+            }
+            {
+
+            }
+           List<string> lista = new List<string>() { "banán", "alma", "körte", "narancs", "kiwi" };
+            int index = lista.IndexOf("körte");
+            if (index != -1) Console.WriteLine("Van körte, indexe:" + index);
             */
+          List<int> lista = new List<int>() { 20 ,31, 10};
+            int[] tömb = lista.ToArray();
+            foreach ( item in tömb)
+           
+            {
+                Console.WriteLine(item);
+            }
             {
                 Console.ReadKey();
             }
