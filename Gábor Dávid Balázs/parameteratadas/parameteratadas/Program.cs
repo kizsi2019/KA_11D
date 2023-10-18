d3707538498c306cc7114ecb5191732031677871
Gábor Dávid <davidbalazs.gabor@katonaj-mkszig.hu>
2023-10-18 13:09:59 +0200

    csharp

---
+++ Gábor Dávid Balázs/parameteratadas/parameteratadas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parameteratadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> regilista = new List<int>() { 20, 31, 10 };
+            List<int> ujlista = regilista;
+            ujlista.Add(40);
+            Console.WriteLine("Régilista tartalma: ");
+            foreach (int item in regilista)
+            {
+                Console.WriteLine(item + ", ");
+            }
+            Console.WriteLine("\nÚjlista tartalma: ");
+            foreach (int item in ujlista)
+            {
+                Console.WriteLine(item + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
