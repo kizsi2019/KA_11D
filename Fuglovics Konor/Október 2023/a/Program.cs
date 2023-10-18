1806c01144094dc33fd8fd0d46f13557c0cf7ffb
SoundCrafterYT <konor.fuglovics@gmail.com>
2023-10-18 08:46:32 +0200

    VSCode Október 18

---
+++ Fuglovics Konor/Október 2023/a/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osszetett
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          /*List<int> Vlist = new List<int>(8);
+            Vlist.Add(24);
+            Vlist.Add(36);
+            Vlist.Add(5);
+            for (int i=0; i<Vlist.Count; i++)
+            {
+                Console.WriteLine(Vlist[i]);
+            }
+            foreach (var item in Vlist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Capacity: {Vlist.Capacity}");
+            Console.WriteLine($"Size: {Vlist.Count}");*/
+
+            List<int> lista = new List<int>();
+            lista.Add(45); lista.Add(23); lista.Add(1);
+            lista.Insert(1, 53);
+
+            Console.ReadKey();
+        }
+    }
+}
