b5295a76eaf10c081637b52711521c548caf6fe2
Adam Balogh <adam.balogh@katonaj-mkszig.hu>
2023-10-19 11:27:50 +0200

    nesze33

--- Balogh Ádám István/csharp/lancoltlista/lancoltlista/Program.cs
+++ Balogh Ádám István/csharp/lancoltlista/lancoltlista/Program.cs
@@ -10,15 +10,31 @@ namespace lancoltlista
     {
         static void Main(string[] args)
         {
-            LinkedList<string> láncoltlista = new LinkedList<string>();
+            /*LinkedList<string> láncoltlista = new LinkedList<string>();
             láncoltlista.AddLast("vár");
             láncoltlista.AddLast("kávé");
             láncoltlista.AddFirst("autó");
+            //láncoltlista.RemoveFirst();
+            LinkedListNode<string> csomópont = lancoltlista.First;
             foreach  (string item in láncoltlista)
             {
                 Console.WriteLine(item);
             }
+            Console.ReadKey();*/
+            Dictionary<string, string> szótár = new Dictionary<string, string>()
+            {
+                { "szín1", "red" },
+                { "szín2", "blue" }
+
+            };
+            if (szótár.ContainsKey("blue")) Console.WriteLine("Van kék");
+            else Console.WriteLine("Nincs kék");
+            foreach (KeyValuePair<string, string> item in szótár)
+            {
+                Console.WriteLine("Kulcs: {0}, értéke: {1}", item.Key, item.Value);
+            }
             Console.ReadKey();
         }
+
     }
 }
