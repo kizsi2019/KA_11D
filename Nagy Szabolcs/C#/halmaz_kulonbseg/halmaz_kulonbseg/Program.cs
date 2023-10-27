162b2bf4c24fe07ed1cf9cb7f35bb5b9ee84e4a7
Diak <diak@katonaj-mkszig.hu>
2023-10-27 10:39:11 +0200

    adta a jó isten

---
+++ Nagy Szabolcs/C#/halmaz_kulonbseg/halmaz_kulonbseg/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace halmaz_kulonbseg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1 = new HashSet<int>() { 1, 2, 3, 4, 6};
+            HashSet<int> set2 = new HashSet<int>() { 2, 4, 7, 5, 8, 9, 10};
+
+            var difference = set1.Except(set2);
+
+
+            Console.Write("Két halmaz különbsége: ");
+            foreach (var item in difference)
+            {
+                Console.Write("{0}, ", item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Nagy Szabolcs/C#/tomb2/tomb2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomb2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] lista = new int[2, 2];
+
+            for (int i = 0; i < lista.GetLength(0); i++)
+                {
+                    for (int j = 0; j < lista.GetLength(1); j++)
+                    {
+                        Console.Write("Adj meg számokat! ");
+                        lista[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+            }
+
+            /*
+            foreach (int i in lista)
+            {
+                Console.WriteLine(i);
+
+            }
+            */
+
+            /*
+            int osszeg = 0;
+            foreach (int i in lista) osszeg = osszeg + i;
+
+            Console.WriteLine();
+            Console.WriteLine("Az összeg: " + osszeg);
+            Console.ReadKey();
+            */
+
+            /*
+            int legkisebb = lista[0, 0];
+            int legnagyobb = lista[0, 0];
+
+            foreach (int item in lista)
+            {
+                if (item < legkisebb)
+                {
+                    legkisebb = item;
+                }
+
+                if (item > legnagyobb)
+                {
+                    legnagyobb = item;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("A legnagyobb szám: " + legnagyobb);
+            Console.WriteLine("A legkisebb szám: " + legkisebb);
+            */
+
+            int szamlalo = 0;
+            foreach (int item in lista)
+            {
+                if (item == 0)
+                {
+                    szamlalo++;
+                }
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(szamlalo);
+            Console.ReadKey();
+
+        }
+    }
+}
