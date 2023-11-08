616dd504aca984dad4383f884d347c7863cf369a
Gábor Dávid <davidbalazs.gabor@katonaj-mkszig.hu>
2023-11-08 08:44:50 +0100

    dolgozat

---
+++ Gábor Dávid Balázs/chsarp/Dolgozat/Dolgozat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. feladat
+            Console.WriteLine("Add meg a nevét: ");
+            string nev = Console.ReadLine();
+
+            Console.WriteLine("Kérem adja meg a jegyeit! ");
+            string jegyek = Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
+
