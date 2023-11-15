4723b7da190af3dbe12b31d8c28b3b32a36f4d6e
Levi <levjuce2004@gmail.com>
2023-11-15 09:37:08 +0100

    dolgozat

---
+++ Vicsápi Levente/C#/meg_egy_pici_gyak/meg_egy_pici_gyak/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meg_egy_pici_gyak
+{
+    internal class Program
+    {
+        public void Valami<T>(IEnumerable<T> lista) 
+        { 
+            foreach (T item in lista) 
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program progi = new Program();
+
+            List<string> lista = new List<string>() {"nigör", "csumi"};
+            List<int> lista2 = new List<int>() {1, 2, 3, 4 ,5};
+
+            int[] tömb = new int[] {1, 2,3, 4, 5};
+            string[] tömb2 = new string[] {"nigör", "csumi" };
+
+            progi.Valami(lista);
+            Console.WriteLine();
+            progi.Valami(lista2);
+            Console.WriteLine();
+            progi.Valami(tömb);
+            Console.WriteLine();
+            progi.Valami(tömb2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vicsápi Levente/C#/sajat_fuggveny_gyak2/sajat_fuggveny_gyak2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sajat_fuggveny_gyak2
+{
+    internal class Program
+    {
+        public void Kivonas(int[] arr)
+        {
+            int kulonbseg = arr[0] * 2;
+            foreach (int item in arr) 
+            {
+                kulonbseg -= item;
+            }
+
+            Console.WriteLine(kulonbseg);
+        }
+        static void Main(string[] args)
+        {
+            int[] szamok = new int[] {10, 5, 2};
+
+            Program progi = new Program();
+
+            progi.Kivonas(szamok);
+
+            Console.ReadKey();
+        }
+    }
+}
