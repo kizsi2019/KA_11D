34048f9e4ab6b4b51e15f60959d6d026a328b007
Adam Balogh <adam.balogh@katonaj-mkszig.hu>
2024-01-17 08:33:45 +0100

    bajbanahaza

--- Balogh Ádám István/csharp/BukkMaraton2019/BukkMaraton2019/Program.cs
+++ Balogh Ádám István/csharp/BukkMaraton2019/BukkMaraton2019/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BukkMaraton2019
 {
@@ -10,6 +11,15 @@ namespace BukkMaraton2019
     {
         static void Main(string[] args)
         {
+
+            //3. feladat
+            List<Versenzyo> versenyzok = new List<Versenzyo>();
+            foreach (var sor in File.ReadAllLines("bukkmaraton2019.txt").Skip(1)) 
+            {
+                versenyzok.Add(new Versenzyo(sor));
+            }
+
+            Console.WriteLine();
         }
     }
 }
