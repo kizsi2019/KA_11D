58ffb98d6f0479d809184d99f7c742cb74217c41
payloaddrone <zsomborkissfodor@gmail.com>
2024-01-17 08:45:22 +0100

    ámongusszi

---
+++ Jankovics Dávid/C#/BukkMarathon2019/BukkMarathon2019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BukkMarathon2019
+{
+    internal class Versenyzo
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
