da8dbd406cbbf310d6cf5ee02effd396e8aa5021
payloaddrone <zsomborkissfodor@gmail.com>
2024-01-17 09:51:04 +0100

    fasza github

--- Kiss-Fodor Zsombor/bükk keriversi/Informatikai_ism_forrasok_K2211/2_feladat/BukkMaraton2019/BukkMaraton2019/Program.cs
+++ Kiss-Fodor Zsombor/bükk keriversi/Informatikai_ism_forrasok_K2211/2_feladat/BukkMaraton2019/BukkMaraton2019/Program.cs
@@ -11,6 +11,7 @@ namespace BukkMaraton2019
     {
         static void Main(string[] args)
         {
+            //4. feladat
             List<Versenyzo> versenyzok = new List<Versenyzo>();
             foreach (var sor in File.ReadAllLines("bukkm2019.txt").Skip(1))
             {
@@ -19,6 +20,30 @@ namespace BukkMaraton2019
 
 
             Console.WriteLine($"4.feladat: Versenytávot nem teljesítők: {(1 - versenyzok.Count / 691.0) * 100}%");
+
+            //5. feladat
+            int noiversenzokRovidTavon = 0;
+            foreach (var v in versenyzok)
+            {
+                if (v.NoiVersenyzo && v.Tav == "Rövid")
+                {
+                    noiversenzokRovidTavon++;
+                }
+            }
+
+            Console.WriteLine($"5. feladat: Női versenyzők száma a rövidtávú versenyen: {noiversenzokRovidTavon}");
+
+            //6. feladat
+            bool tobbMintHat = false;
+            foreach (var v in versenyzok)
+            {
+                if (v.tobbMintHat)
+                {
+                    tobbMintHat = true;
+                    break;
+                }
+            }
+            Console.WriteLine($"6. feladat: {(tobbMintHat ? "Volt" : "Nem Volt")} Ilyen versenyző");
 
             Console.ReadKey();
         }
