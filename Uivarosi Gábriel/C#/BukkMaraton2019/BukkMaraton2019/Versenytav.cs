72c082ea44195494d48ab146f0e0f8adf75ae589
Gabi455 <90388665+Gabi4555@users.noreply.github.com>
2024-01-17 09:01:45 +0100

    Don't Commit ass im my MAPPA!!!!

--- Uivarosi Gábriel/C#/BukkMaraton2019/BukkMaraton2019/Versenytav.cs
+++ Uivarosi Gábriel/C#/BukkMaraton2019/BukkMaraton2019/Versenytav.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,40 +33,3 @@ namespace BukkMaraton2019
 
     }
 
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BukkMaraton2019
-{
-     public class Versenytav
-    {
-       
-            private string Rajtszam;
-            public string Tav
-            {
-                get
-                {
-                    switch (Rajtszam[0])
-                    {
-                        case 'M': return "Mini";
-                        case 'R': return "Rövid";
-                        case 'K': return "Közép";
-                        case 'H': return "Hosszú";
-                        case 'E': return "Pedelec";
-                    }
-                    return "Hibás rajtszám";
-                }
-            }
-            public Versenytav(string rajtszam)
-            {
-                Rajtszam = rajtszam;
-            }
-        }
-
-    }
-
->>>>>>> 0758e27e55a72f5ac8be1e4e1e816bf86f6b12b5
