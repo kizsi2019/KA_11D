212c0a8f34b3cbc3f875e56d17aca66a912bb5cd
Levi <levjuce2004@gmail.com>
2024-01-17 09:02:48 +0100

    itt van

---
+++ Vicsápi Levente/C#/Bukkmaraton2019/Bukkmaraton2019/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Bukkmaraton2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new List<Versenyzo>();
+
+            foreach (var sor in File.ReadAllLines("bukkm2019.txt").Skip(1)) 
+            {
+                versenyzok.Add(new Versenyzo(sor));
+            }
+
+            Console.WriteLine($"4.feladat: Versenytávot nem teljesítők: {(1 - versenyzok.Count / 691.0) * 100}%");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vicsápi Levente/C#/heritance/heritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Szuloosztaly szulo = new Szuloosztaly();
+            szulo.Metodus();
+            
+            Leszarmazott gyerek = new Leszarmazott();
+            gyerek.Metodus();
+            gyerek.UjMetodus();
+
+            Console.ReadKey();
+        }
+    }
+}
