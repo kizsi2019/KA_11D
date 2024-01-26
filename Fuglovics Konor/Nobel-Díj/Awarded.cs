62df9705e95857d3ce255095d4612ae7b7dd3cf0
SoundCrafterYT <konor.fuglovics@gmail.com>
2024-01-26 13:37:05 +0100

    nobel

---
+++ Fuglovics Konor/Nobel-Díj/Awarded.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel_Díj
+{
+    internal class Awarded
+    {
+        public int Year { get; private set; }
+        public string Name { get; private set; }
+        public string BirthDeath { get; private set; }
+        public string CountryCode { get; private set; }
+
+        public Awarded(string row)
+        {
+            string[] m = row.Split(';');
+            Year = int.Parse(m[0]);
+            Name = m[1];
+            BirthDeath = m[2];
+            CountryCode = m[3];
+        }
+    }
+}
---
+++ Fuglovics Konor/Nobel-Díj/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel_Díj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Awarded> awarded_people = new List<Awarded>();
+            foreach (var i in File.ReadAllLines("orvosi_nobeldijak.txt").Skip(1))
+            {
+                awarded_people.Add(new Awarded(i));
+            }
+            Console.WriteLine($"The number of awarded people: {awarded_people.Count()}");
+            Console.ReadKey();                                                                                                                                                                                                                                                              
+        }
+    }
+}
