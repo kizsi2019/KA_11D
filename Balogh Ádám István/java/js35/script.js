45dd3223f087d4c35f9ead96a7d958df58736317
Adam Balogh <adam.balogh@katonaj-mkszig.hu>
2024-01-26 12:29:02 +0100

    ja

---
+++ Balogh Ádám István/java/js35/script.js
@@ -0,0 +1,25 @@
+var evek = [1954, 1990, 1963, 2000, 2010];
+
+function tombMuvelet(tomb, fv) {
+    var eredmeny = [];
+
+
+    for (var i = 0; i < tomb.length; i++) {
+        eredmeny.push(fv(tomb[i]));
+    }
+    return eredmeny;
+}
+
+function korszamitas(elem) {
+    return 2024 - elem;
+}
+
+function felnott(elem) {
+    return elem >= 18;
+}
+
+var korok = tombMuvelet(evek, korszamitas);
+console.log(korok);
+
+var felnottek = tombMuvelet(korok, felnott);
+console.log(felnottek);
