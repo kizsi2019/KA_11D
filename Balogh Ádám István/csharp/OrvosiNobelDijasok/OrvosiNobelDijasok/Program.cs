16be58446a691a4fd20c8a43f9d809ca7f4363e4
Adam Balogh <adam.balogh@katonaj-mkszig.hu>
2024-01-26 13:22:52 +0100

    dsd

---
+++ Balogh Ádám István/csharp/OrvosiNobelDijasok/OrvosiNobelDijasok/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OrvosiNobelDijasok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.feladat
+            List<Dijazott> dijazottak = new List<Dijazott>();
+            foreach (var i in File.ReadAllLines("orvosi_nobeldijak.txt").Skip(1))
+            {   
+                dijazottak.Add(new Dijazott(i));
+            }
+            // 3. feladat
+            Console.WriteLine($"3. feladat: A díjazottak száma: {dijazottak.Count} fő");
+
+            Console.ReadKey();
+        }
+    }
+}
