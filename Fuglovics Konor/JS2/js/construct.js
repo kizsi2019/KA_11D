efef8bbca2d4bc46ff806151902fc76cb0450eb8
SoundCrafterYT <konor.fuglovics@gmail.com>
2024-01-26 11:19:22 +0100

    valamiért nem megy a tömb.length

--- Fuglovics Konor/JS2/js/construct.js
+++ Fuglovics Konor/JS2/js/construct.js
@@ -17,7 +17,7 @@ Person.prototype.AgeCalculate = function(){
     console.log(2024 - this.dateofbirth);
 };
 
-Person.prototype.test = "test";
+/*Person.prototype.test = "test";
 
 var Ödön = new Person('Ödön', 1978, 'graphic designer');
 Ödön.AgeCalculate();
@@ -29,4 +29,22 @@ console.log(Person);
 console.log(Ödön); 
 console.log(Ödön.test);
 console.log(Person.prototype);
-console.log(Ödön.hasOwnProperty('name'));
+console.log(Ödön.hasOwnProperty('name'));*/
+
+var PersonProto = {
+    AgeCalculate: function(){
+        console.log(2024-this.dateofbirth);
+    }
+};
+
+var Ödön = Object.create(PersonProto);
+
+Ödön.name = "Ödön";
+Ödön.dateofbirth = 1960;
+Ödön.work = "baker";
+
+var Kati = Object.create(PersonProto, {
+    name: {value: "Kati"},
+    dateofbirth: {value: 1990},
+    work: {value: "cosmetic"}
+});
---
+++ Fuglovics Konor/JS4/js/construct.js
@@ -0,0 +1,24 @@
+var years = [1945, 1985, 2001, 2005, 2016];
+
+function BlockCalculation(block, fv){
+    var result = [];
+
+    for (var i = 0; i < block.length; i++){
+        result.push(fv(block[i]));
+    }
+    return result;
+}
+
+function AgeCalculate(elem){
+    return 2024 - elem;
+}
+
+function adult(elem){
+    return elem >= 18;
+}
+
+var ages = BlockCalculation(ages, AgeCalculate);
+console.log(ages);
+
+var adults = BlockCalculation(ages, AgeCalculate);
+console.log(adults);
