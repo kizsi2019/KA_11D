62e73e43fe53fb5077b2495896454949b1814f69
AlexVock <aisystreams@gmail.com>
2024-01-31 13:27:29 +0100

    mivanmivan

---
+++ Tukarcs Alex/C#/orvosi nobeldijasok/orvosi nobeldijasok/elethossz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orvosi_nobeldijasok
+{
+    class Elethossz
+    {
+        private int Tol { get; set; }
+        private int Ig { get; set; }
+        public int ElethosszEvekben => Tol == -1 || Ig == -1 ? -1 : Ig - Tol;
+
+        public bool IsmertAzElethossz => ElethosszEvekben != -1;
+
+        public Elethossz(string SzuletesHalalozas)
+        {
+            string[] m = SzuletesHalalozas.Split('-');
+            try
+            {
+                Tol = int.Parse(m[0]);
+            }
+            catch (Exception)
+            {
+                Tol = -1;
+            }
+            try
+            {
+                Ig = int.Parse(m[1]);
+            }
+            catch (Exception)
+            {
+                Ig = -1;
+            }
+        }
+    }
+}
