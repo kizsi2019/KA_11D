b1e36b109e949c956903f9726e578ac67ff70d7a
Fekete Zoltan <zoltan.fekete@katonaj-mkszig.hu>
2024-01-31 14:59:56 +0100

    karacsony

---
+++ Fekete Zoltán/csharp/karacsonyCLI/karacsonyCLI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karacsonyCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<NapiMunka> diszek = new List<NapiMunka>();
+            foreach (string sor in File.ReadAllLines("diszek.txt"))
+            {
+                diszek.Add(new NapiMunka(sor));
+            }
+            // 4. feladat
+            Console.WriteLine($"4. feladat: Összesen {NapiMunka.KeszultDb} darab dísz készült.");
+            
+            Console.ReadKey();
+        }
+    }
+}
