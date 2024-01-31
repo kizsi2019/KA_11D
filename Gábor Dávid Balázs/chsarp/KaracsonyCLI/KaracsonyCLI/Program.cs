30c0e8562edc509226d8a88cc305439b8363a875
Gábor Dávid <davidbalazs.gabor@katonaj-mkszig.hu>
2024-01-31 14:59:08 +0100

    karacsony

---
+++ Gábor Dávid Balázs/chsarp/KaracsonyCLI/KaracsonyCLI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace KaracsonyCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<NapiMunka> napimunkalista = new List<NapiMunka>();
+            foreach (string sor in File.ReadAllLines("diszek.txt"))
+            {
+                napimunkalista.Add(new NapiMunka(sor));
+            }
+
+            Console.WriteLine($"4. feladat: Összesen {NapiMunka.KeszultDb} darab dísz készült el");
+            Console.ReadKey();
+        }
+    }
+}
