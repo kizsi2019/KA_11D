761585943a558945269f120ce87f52fadd4450be
HighAfRizs <95404359+HighAfRizs@users.noreply.github.com>
2024-02-07 17:10:42 +0100

    toto

---
+++ Kispál Marcell/toto/TotoData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toto
+{
+    internal class TotoData
+    {
+        public int Ev { get; set; }
+        public int Het { get; set; }
+        public int Fordulo { get; set; }
+        public double T13p1 { get; set; }
+        public double Ny13p1 { get; set; }
+        public string Eredmenyek { get; set; }
+
+        public TotoData(string line)
+        {
+            string[] splitted = line.Split(';');
+            Ev = int.Parse(splitted[0]);
+            Het = int.Parse(splitted[1]);
+            Fordulo = int.Parse(splitted[2]);
+            T13p1 = double.Parse(splitted[3]);
+            Ny13p1 = double.Parse(splitted[4]);
+            Eredmenyek = (splitted[5]);
+        }
+    }
+}
