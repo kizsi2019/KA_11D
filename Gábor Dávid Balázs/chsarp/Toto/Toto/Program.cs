b0a402f62701990cae57ad58495d41b58a7c9b23
Gábor Dávid <davidbalazs.gabor@katonaj-mkszig.hu>
2024-02-07 13:56:12 +0100

    toto

---
+++ Gábor Dávid Balázs/chsarp/Toto/Toto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Toto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. feladat
+            List<EredmenyElemzo> eredmenyelemzo = new List<EredmenyElemzo>();
+            foreach (var i in File.ReadAllLines("toto.txt").Skip(1))
+            {
+                eredmenyelemzo.Add(new EredmenyElemzo(i));
+            }
+            // 3. feladat
+            Console.WriteLine($"3. feladat: Fordulók száma: {eredmenyelemzo.Count}");
+
+            // 4. feladat
+
+            Console.ReadKey();
+        }
+    }
+}
