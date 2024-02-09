43266798828f871850d2b91fb640b641c9ed44b8
Levi <levjuce2004@gmail.com>
2024-02-09 18:13:16 +0100

    összes munkám

--- Vicsápi Levente/html/callback_függvények/evek_pelda.js
+++ Vicsápi Levente/html/callback_függvények/evek_pelda.js
@@ -27,6 +27,7 @@ console.log(felnottek);
 
 //Visszaadott függvények
 
+/*
 function interjuKerdes(foglalkozas){
     if (foglalkozas == 'tanar'){
         return function(nev){
@@ -59,3 +60,106 @@ kerdesEladoknak('Ági');
 
 interjuKerdes('tanar')('Péter');
 interjuKerdes('programozó')('Ödön');
+*/
+
+/*
+function jatek(){
+    var pont = Math.random() * 10;
+    console.log(pont >= 5);
+}
+
+jatek();
+(function(){
+    var pont = Math.random() * 10;
+    console.log(pont >= 5);
+})();
+
+(function(teszt){
+    var pont = Math.random() * 10;
+    console.log(pont >= 5);
+    console.log(teszt);
+})('Hello');
+*/
+
+//Clousure-ök
+
+
+function nyugdij(ev){
+    var szoveg = "A nyugdíjazásig hátralévő évek szánma: "
+    return function(szuletisiEv){
+        var datumObjektum = new Date();
+        var aktualisEv = datumObjektum.getFullYear();
+        var kor = aktualisEv - szuletisiEv;
+        console.log(szoveg + (ev - kor))
+    }
+}
+
+
+
+var nyugdijazasUSA = nyugdij(66);
+nyugdij(66)(2004);
+
+
+/* Clousure összefoglaló
+Egy belső függvény mindig képes hozzáférni az őt tartalmazó
+külső függvény paramétereihez és változóhoz, még azután is,
+hogy a külső függvény befejezte futásait
+*/
+
+
+var nyugdijazasHUN = nyugdij(65);
+var nyugdijazasIZL = nyugdij(67);
+nyugdijazasHUN(2004);
+nyugdijazasIZL(2004);
+
+/*
+var szamlalo = 0;
+
+function leptet(){
+    szamlalo++;
+    console.log(szamlalo);
+}*/
+
+/*
+var szamlalo = 0;
+function leptet(){
+    var szamlalo = 0;
+    szamlalo++;
+    console.log(szamlalo);
+}
+*/
+
+/*
+var szamlalo = 0;
+function leptet(){
+    var szamlalo = 0;
+
+    return function(){
+        szamlalo++;
+        console.log(szamlalo);
+    }
+}
+
+var hozzaad = leptet();
+*/
+/*
+hozzaad();
+hozzaad();
+hozzaad();
+*/
+
+
+var leptet = (
+    function(){
+        var szamlalo = 0;
+
+        return function(){
+            szamlalo++;
+            console.log(szamlalo);
+        }
+    }
+)();
+
+leptet();
+leptet();
+leptet();
