07d10e10cc503f10c39793e4c25a069520e4dfbf
Szakaly Abel <abel.szakaly@katonaj-mkszig.hu>
2024-02-14 14:15:45 +0100

    olimpiasql

--- Szakály Ábel/csharp/karacsonyCLI/karacsonyCLI/Program.cs
+++ Szakály Ábel/csharp/karacsonyCLI/karacsonyCLI/Program.cs
@@ -59,7 +59,7 @@ namespace karacsonyCLI
 
             Console.Write("7. Feladat: a legtobbet eladott dísz");
             Dictionary<string, int> eladottak = new Dictionary<string, int>();
-            eladottak.Add("harang", 0);
+            eladottak.Add("Harang", 0);
             eladottak.Add("Angyalka", 0);
             eladottak.Add("Fenyőfa", 0);
             foreach(Napimunka dísz in napiMunkaLista)
@@ -70,7 +70,28 @@ namespace karacsonyCLI
             }
             int max = eladottak.Values.Max();
             Console.WriteLine();
+            foreach(var item in eladottak)
+            {
+                if (item.Value == max)
+                {
+                    Console.WriteLine($"\t{item.Key}");
+                }
+            }
 
+            Console.WriteLine();
+
+            StreamWriter sw = new StreamWriter("bevétel.txt");
+            int legalabb10000 = 0;
+            foreach (Napimunka dísz in napiMunkaLista)
+            {
+                if (dísz.NapiBevetel() >= 10000)
+                {
+                    sw.WriteLine($"{dísz.Nap} - {dísz.NapiBevetel()}");
+                    legalabb10000++;
+                }
+            }
+            sw.WriteLine($"{legalabb10000} napon volt legalább 10000 forint a bevétel. \n");
+            sw.Close();
 
             Console.ReadKey();
         }
