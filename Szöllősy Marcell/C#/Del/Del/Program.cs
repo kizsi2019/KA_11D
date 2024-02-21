1ccfe36afa04115ff6c97f7a57ccb5733b3b405d
Marcell Szollosy <marcell.szollosy@katonaj-mkszig.hu>
2024-02-21 09:40:11 +0100

    nem

---
+++ Szöllősy Marcell/C#/Del/Del/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    delegate void voidDelegate();
+    static void Metódus();
+    {
+    Console.WriteLine("A metódus lefutott");
+    }
+    static void Main(string[] args)
+    {
+    voidDelegate vd = new voidDelegate(Metódus);
+    Console.ReadKey();
+     }
+}
