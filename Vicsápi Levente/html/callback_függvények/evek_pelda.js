b935a6119472f0e34afa4962d35ac8a83ffabd36
meszaros16 <levjuce2004@gmail.com>
2024-02-21 08:08:31 +0100

    minden munka

--- Vicsápi Levente/html/callback_függvények/evek_pelda.js
+++ Vicsápi Levente/html/callback_függvények/evek_pelda.js
@@ -95,7 +95,7 @@ function nyugdij(ev){
 }
 
 
-
+/*
 var nyugdijazasUSA = nyugdij(66);
 nyugdij(66)(2004);
 
@@ -106,11 +106,12 @@ külső függvény paramétereihez és változóhoz, még azután is,
 hogy a külső függvény befejezte futásait
 */
 
-
+/*
 var nyugdijazasHUN = nyugdij(65);
 var nyugdijazasIZL = nyugdij(67);
 nyugdijazasHUN(2004);
 nyugdijazasIZL(2004);
+*/
 
 /*
 var szamlalo = 0;
@@ -148,7 +149,7 @@ hozzaad();
 hozzaad();
 */
 
-
+/*
 var leptet = (
     function(){
         var szamlalo = 0;
@@ -163,3 +164,75 @@ var leptet = (
 leptet();
 leptet();
 leptet();
+*/
+
+/*
+var odon = {
+    nev: 'Ödön',
+    kor: 45,
+    foglalkozás: 'csillagász',
+    udvozlet: function(stilus, napszak){
+        if (stilus === 'hivatalos'){
+            console.log('Üdvözlöm, jó ' + napszak + ' kívánok! ' + this.nev + ' vagyok');
+        } else if (stilus === 'baráti'){
+            console.log('Szia, jó ' + napszak + '!');
+        }
+    }
+}
+
+odon.udvozlet('hivatalos', 'hajnalt');
+odon.udvozlet('baráti', 'estét');
+
+var bela = {
+    nev: 'Béla',
+    kor: 62,
+    foglalkozás: 'portás'
+}
+*/
+
+/*
+odon.udvozlet.call(bela, 'baráti', 'estét');
+
+odon.udvozlet.apply(bela, ['baráti', 'estét']);
+*/
+
+/*
+var odonBarati = odon.udvozlet.bind(odon, 'baráti');
+odonBarati('napot');
+odonBarati('estét');
+*/
+
+/*
+var belaHivatalos = odon.udvozlet.bind(bela, 'hivatalos');
+var belahivatalosReggeli = odon.udvozlet.bind(bela, 'hivatalos', 'reggelt');
+
+belaHivatalos('estét');
+belahivatalosReggeli();
+*/
+
+var evek = [1954, 1990, 1963, 2000, 2010];
+
+function tombMuvelet(tomb, fv){
+    var eredmeny = [];
+
+    for (var i = 0; i < tomb.length; i++){
+        eredmeny.push(fv(tomb[i]));
+    }
+    return eredmeny;
+};
+
+function korszamitas(elem){
+    return 2024 - elem;
+};
+
+function felnott(korhatar, elem){
+    return elem >= korhatar;
+};
+
+var korok = tombMuvelet(evek, korszamitas);
+console.log(evek);
+console.log(korok);
+
+
+var felnottkorJapanban = tombMuvelet(korok, felnott.bind(this, 20));
+console.log(felnottkorJapanban);
