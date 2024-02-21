b632be9369d48ce2a9ea1b2ad1f4ac81ace1209b
alkhayyat ali <alkhayyatalii@gmail.com>
2024-02-21 09:30:57 +0100

    delegacio

---
+++ Al-Khayyat Anaam Ali/java1/Delegate/Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate11
+{
+    internal class Program
+    {
+        delegate void VoiDelegate(); //a delegate létrehozása
+        static void Metódus()
+        {
+            Console.WriteLine("A metódus lefutott!");
+        }
+        static void Main(string[] args)
+        {
+            VoiDelegate vd = new VoiDelegate(Metódus); // létrejön a hivatkozás
+            vd();// meghívja a megadott metódust, amire hivatkozik
+        }
+    }
+}
