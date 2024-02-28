831272ad040b9345db1d0664d105007c4384fefc
Jakab Mate Tibor <tibormate.jakab@katonaj-mkszig.hu>
2024-02-28 13:20:59 +0100

    mai

---
+++ Jakab Máté Tibor/C#/2023.12.13 c#/2023.12.13 c#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023._12._13_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Beolvasás
+            Streamreader sr = new Streamreader("végeredmény.txt");
+            List<string> eredmény = new List<string>();
+            while (!sr.EndofStream) {
+            eredmény.Add(sr.ReadLine());
+            }
+            sr.Close();
+            //kiválasztás tétele
+            Console.WriteLine("Adja meg a versenyző nevét ");
+            string név = Console.ReadLine();
+            int i = 0;
+            while (i < eredmény.Count && eredmény[i] ! = név)
+            {
+                i++;
+            }
+            Console.WriteLine("{0} nevű versenyző {1} lett", név, i + 1);
+            Console.ReadKey();
+        }
+    }
+}
