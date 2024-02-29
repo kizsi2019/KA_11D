a92bafba15a810604f61e03f7e0c9eac919eb66c
Jankovics David <david.jankovics@katonaj-mkszig.hu>
2024-02-29 10:58:52 +0100

    sanyi_CRUD

---
+++ Jankovics Dávid/adatbázis/SQLiteExtra2.py
@@ -0,0 +1,32 @@
+
+# forrás: https://docs.python.org/3/library/sqlite3.html
+import sqlite3
+
+con = sqlite3.connect("tutorial2.db")
+cur = con.cursor()
+  
+cur.execute("drop table if exists szgk")
+# Megadhatók pl. MySQL/MariaDB típusok is a tábla létrehozásánál
+# lásd 3.1.1: https://www.sqlite.org/datatype3.html#type_affinity
+cur.execute('''
+    CREATE TABLE szgk(
+            id INT, 
+            tipus VARCHAR(20), 
+            modell VARCHAR(20), 
+            rendszam VARCHAR(15), 
+            ajtok_szama INT,
+            gyartas_eve DATE
+        );
+''')
+cur.execute('''
+    INSERT INTO szgk (id, tipus, modell, rendszam, ajtok_szama, gyartas_eve)
+    VALUES(10, 'Opel', 'Corsa C', 'AAA123', 4, '2002-10-03') ,
+    ('sanyi','sanyi', 'sanyi', 'sanyi', 'sanyi', 'sanyi' )
+''')
+con.commit()
+
+for row in cur.execute("SELECT * FROM szgk"):
+    print(row)
+
+con.close()    
+  
