9931d0d602f15a641428bfa9477525be2c894bde
SoundCrafterYT <konor.fuglovics@gmail.com>
2024-03-01 11:56:24 +0100

    Java

---
+++ Fuglovics Konor/koltsegvetes-js-projekt/js/app.js
@@ -0,0 +1,30 @@
+//Költségvetés vezérlő
+var budget_manager = (function(){
+
+})();
+//Surface manager
+var surface_manager = (function(){
+
+})();
+//App manager
+var manager = (function(BudgetMan, SurfMan){
+    var ManTitleAdd = function(){
+        // 1 - Capturing imported data
+
+        // 2 - Giving the data to the budget manager module
+
+        // 3 - Appearance in UI
+
+        // 4 - Re-calculating the budget
+
+        // 5 - Total amount on the interface
+        console.log("It works!")
+    }
+    document.querySelector('.hozzaad__gomb').addEventListener('click', ManTitleAdd);
+    document.addEventListener('keydown', function(event){
+        if (event.keyCode === 13)
+        {
+            ManTitleAdd();
+        }
+    })
+})(budget_manager, surface_manager);
