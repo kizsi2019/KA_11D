d1b326b564e60e88a7c4ada610ba7871d6871d66
Jankovics David <david.jankovics@katonaj-mkszig.hu>
2024-03-08 11:16:40 +0100

    ahhh

--- Jankovics Dávid/js/js5/script.js
+++ Jankovics Dávid/js/js5/script.js
@@ -1,15 +1,15 @@
 //visszaadott függvények
 function interjuKerdes(foglalkozas) {
     if (foglalkozas == 'tanár') {
-        return function(nev) {
+        return function (nev) {
             console.log(nev + ', meg tudná mondani, hogy milyen tárgyakat tanít?')
         }
-    }else if (foglalkozas == 'elado') {
-        return function(nev) {
+    } else if (foglalkozas == 'elado') {
+        return function (nev) {
             console.log(nev + ', hogyan kezel egy vevő reklamációt?')
         }
     } else {
-        return function(nev) {
+        return function (nev) {
             console.log('mi a foglalkozása kedves ' + nev + '?')
         }
     }
