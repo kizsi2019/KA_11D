1ab0fa3fedd9018a8021feb8893d562bf79c9259
Gábor Dávid <davidbalazs.gabor@katonaj-mkszig.hu>
2024-03-08 10:38:16 +0100

    .

--- Gábor Dávid Balázs/java/koltsegvetes-js-projekt/js/app.js
+++ Gábor Dávid Balázs/java/koltsegvetes-js-projekt/js/app.js
@@ -5,14 +5,37 @@ var koltsegvetesVezerlo = (function() {
 
 // Felület vezérlés
 var feluletVezerlo = (function() {
+    var DOMelemek = {
+        inputTipus: '.hozzaad__tipus',
+        inputLeiras: '.hozzaad__leiras',
+        inputErtek: '.hozzaad__ertek',
+        inputGomb: '.hozzaad__gomb',
+    }
+
+    return {
+        getInput: function() {
+            return {
+                tipus: document.querySelector(DOMelemek.inputTipus).value,
+                leiras: document.querySelector(DOMelemek.inputLeiras).value,
+                ertek: document.querySelector(DOMelemek.inputErtek).value
+            }
+        },
+        getDOMelemek: function() {
+            return DOMelemek;
+        }
+    }
 
 })();
 
 // Alkalmazás vezérlés
 var vezerlo = (function(koltsegvetesVez, feluletVez) {
 
+    var DOM = feluletVezerlo.getDOMelemek();
+
     var vezTetelHozzaadas = function() {
         // 1. bevitt adatok megszerzeése
+        var input = feluletVezerlo.getInput();
+        console.log(input);
 
         // 2. adatok átadása a koltsegvetesVezerlo modulnak
 
@@ -28,7 +51,10 @@ var vezerlo = (function(koltsegvetesVez, feluletVez) {
     document.querySelector('.hozzaad__gomb').addEventListener('click', vezTetelHozzaadas);
 
     document.addEventListener('keydown', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
+        if (event.key !== undefined && event.key === 'Enter') {
+            vezTetelHozzaadas();
+        }
+        else if (event.keyCode !== undefined && event.keyCode === 13) {
             vezTetelHozzaadas();
         }
     });
