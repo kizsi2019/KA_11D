a58b03059b02101d6d42be469657bc447b86bd8b
SoundCrafterYT <konor.fuglovics@gmail.com>
2024-03-08 10:41:50 +0100

    SwawS

--- Fuglovics Konor/koltsegvetes-js-projekt2/js/app.js
+++ Fuglovics Konor/koltsegvetes-js-projekt2/js/app.js
@@ -4,12 +4,32 @@ var budget_manager = (function(){
 })();
 //Surface manager
 var surface_manager = (function(){
-
+    var DOMelement = {
+        inputType: '.hozzaad__tipus',
+        inputDescription: '.hozzaad__leiras',
+        inputValue: '.hozzaad__ertek',
+        inputButton: '.hozzaad__gomb'
+    }
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMelement.inputType).value,
+                description: document.querySelector(DOMelement.inputDescription).value,
+                value: document.querySelector(DOMelement.inputValue).value
+            }
+        },
+        getDOMelement: function(){
+            return DOMelement;
+        }
+    }
 })();
 //App manager
 var manager = (function(BudgetMan, SurfMan){
+    var DOM = surface_manager.getDOMelement();
     var ManTitleAdd = function(){
         // 1 - Capturing imported data
+        var input = surface_manager.getInput();
+        console.log(input);
 
         // 2 - Giving the data to the budget manager module
 
@@ -27,10 +47,10 @@ var manager = (function(BudgetMan, SurfMan){
             ManTitleAdd();
             console.log(event);
         }
-        /*if (event.keyCode === 13 && event.which === 13) //Deprecated
+        else if (event.keyCode !== undefined && event.keyCode === 13) //Deprecated
         {
             ManTitleAdd();
             console.log(event);
-        }*/
+        }
     })
 })(budget_manager, surface_manager);
