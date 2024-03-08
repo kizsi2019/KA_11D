52326ce33ba5e846aaa49cf8ee768a2379ccb42b
meszaros16 <levjuce2004@gmail.com>
2024-03-08 10:38:32 +0100

    költségvetés

---
+++ Vicsápi Levente/C#/linq_gyak/linq_gyak/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_gyak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int> { 1, 2, 3, 4, 5 };
+
+            //lista elemeinek kiiratása lambda kifejezéssel
+            //lista.ForEach(x => Console.WriteLine(x));
+
+            int osszeg = lista.Aggregate((x, y) =>  x + y);
+
+            Console.WriteLine($"A lista elemeinek összege: {osszeg}");
+
+            Console.ReadKey();
+        }
+    }
+}
