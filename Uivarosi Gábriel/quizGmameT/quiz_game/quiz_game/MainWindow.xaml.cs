7016fd1e716e7e24cff1c307a2ac05eb7129b6e9
Gabi455 <90388665+Gabi4555@users.noreply.github.com>
2024-03-13 13:37:54 +0100

    gfdgfdgdfgd

---
+++ Uivarosi Gábriel/quizGmameT/quiz_game/quiz_game/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace quiz_game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+             List<string> Bemenet = new List<string>();
+
+            foreach (string line in File.ReadLines("import.txt"))
+            {
+                Bemenet.Add(line);
+            }
+
+            int kérdés_szám = 0;
+
+          //Make Functont to separate
+
+           
+        }
+
+    }
+}
