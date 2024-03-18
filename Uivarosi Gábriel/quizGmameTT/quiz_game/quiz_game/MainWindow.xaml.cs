475d7df02d4536b3693c4b6e35ca5c8300dfcde1
Gabi455 <90388665+Gabi4555@users.noreply.github.com>
2024-03-18 21:58:15 +0100

    Majdnem Kész

---
+++ Uivarosi Gábriel/quizGmameTT/quiz_game/quiz_game/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography.X509Certificates;
+
+namespace quiz_game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+             List<string> Bemenet = new List<string>();
+
+            foreach (string line in File.ReadLines("import.txt"))
+            {
+                Bemenet.Add(line);
+            }
+       
+            int kérdés_szám = 0;
+            int Jól_megadot_válaszok = 0;
+            int megoldás_százalék = 0;
+            string Jelenlegi_Válasz = "";
+            //Make Functont to separate
+            string[] szep1;
+            string Maga_A_kérdés = "";
+            string A_válasz = "";
+            string B_Válasz = "";
+            string C_Válasz = "";
+            string Helyes_Válasz_index = ""; 
+            string Helyes_Válasz_ABC = "";
+             void Szeparálás()
+            {
+                szep1 = Bemenet[kérdés_szám].Split(';');
+                Maga_A_kérdés = szep1[0];
+                A_válasz = szep1[1];
+                B_Válasz = szep1[2];
+                C_Válasz = szep1[3];
+                Helyes_Válasz_index = szep1[4];
+          
+            
+            }
+            Szeparálás();   
+            Ki_írás();
+            void Ki_írás() {
+                X_KérdésTB.Text = kérdés_szám+1 + " "+ "Kérdés";
+                Kérdés_tb.Text = Maga_A_kérdés;
+                Textbox_A.Text = A_válasz;
+                Textbox_B.Text = B_Válasz;
+                Textbox_C.Text = C_Válasz;
+
+
+            }
+
+            bool tesz_kész = false;
+
+            //    private void btnMentes_Click(object sender, RoutedEventArgs e)
+            //    {
+
+            //    }
+
+            Button_A.Click += (sender, e) =>
+            {
+                // Handle button click here
+                Jelenlegi_Válasz = "1";
+               Csekolás();
+            };
+
+            Button_B.Click += (sender, e) =>
+            {
+                // Handle button click here
+                Jelenlegi_Válasz = "2";
+                Csekolás();
+            };
+
+            Button_C.Click += (sender, e) =>
+            {
+                // Handle button click here
+                Jelenlegi_Válasz = "3";
+                Csekolás();
+            };
+
+            void százalék_kiírás()
+            {
+                megoldás_százalék = (Jól_megadot_válaszok / 15) * 100;
+                Correct_ans_text.Text = "A Helyes Válaszok százaléka:";
+              string megoldás_százalék_tx = megoldás_százalék.ToString();
+                Correct_ans_num.Text = megoldás_százalék_tx;
+            }
+
+            void Csekolás()
+            {
+
+                if (Jelenlegi_Válasz == Helyes_Válasz_index && tesz_kész == false)
+                {
+                    Jól_megadot_válaszok++;
+
+                }
+                if (tesz_kész == false)
+                {
+                    kérdés_szám++;
+                    Szeparálás();
+                    Ki_írás();
+                } 
+                
+                if (kérdés_szám >= 14)
+                {
+                    tesz_kész = true;
+                    százalék_kiírás();
+                }
+          
+        }
+
+         
+
+
+        }
+
+
+    }
+
+    }
+
