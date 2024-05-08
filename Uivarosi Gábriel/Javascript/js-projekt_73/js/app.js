d5cd006809d339625c11f12ca5f8c9650e1e6026
Gabi455 <90388665+Gabi4555@users.noreply.github.com>
2024-05-08 13:05:30 +0200

    Undifend

---
+++ Uivarosi Gábriel/Javascript/js-projekt_73/js/app.js
@@ -0,0 +1,153 @@
+// let és const
+// es5
+
+/*
+var nev5 = 'Teszt Elek';
+var kor5 = 40;
+
+nev5 = 'Kiss Pista';
+
+console.log(nev5);
+
+//es5
+ function nyelvVizsga5(siker) {
+    if (siker) {
+        var keresztnev = 'Ödön';
+        var szuletesiEv = 1980;
+        console.log(keresztnev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga');
+    }
+
+}
+
+nyelvVizsga5(true);
+
+//es6
+
+//const nev6 = 'Teszt Elek';
+//let kor6 = 40;
+
+//nev6 = 'Kiss Pista'; // hibát dob
+
+//console.log(nev6);
+
+function nyelvVizsga6(siker) {
+    if (siker) {
+        let keresztNev = 'Ödön';
+        const szuletesiEv = 1980;
+        console.log(keresztNev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga');
+    }
+
+}
+
+nyelvVizsga6(true);
+*/
+
+/*
+let i = 9;
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
+*/
+
+/*
+var i = 9;
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
+*/
+
+// Blokkok és IIFE
+// es6
+/*
+{
+    const a = 1;
+    let b = 2;
+    //var c = 3;
+}
+
+console.log(a + b);
+
+// es5
+
+(function() {
+    let c = 3;
+    console.log(c);
+})();
+
+console.log(c);
+*/
+/*
+let vezeteknev = 'Teszt';
+let keresztnev = 'Elek';
+
+const szuletesiEv = 1970;
+
+function korSzamitas(ev) {
+    return 2024 - ev;
+}
+
+//es5
+
+console.log(vezeteknev + ' ' + keresztnev + ', született ' + szuletesiEv + '-ben. Most ' + korSzamitas(szuletesiEv) + ' éves');
+
+//es6
+
+console.log(`${vezeteknev} ${keresztnev}, született ${szuletesiEv}-ben. Most ${korSzamitas(szuletesiEv)} éves.`);
+
+const nev = `${vezeteknev} ${keresztnev}`;
+
+console.log(nev.startsWith('T')); 
+
+console.log(nev.endsWith('ek'));
+
+console.log(nev.includes('ele'));
+
+console.log(vezeteknev.repeat(3));
+
+console.log(`${vezeteknev} `.repeat(3));
+*/
+/*
+const evek = [1970, 1975, 1954, 2010, 1981];
+
+var korokES5 = evek.map(function(elem){
+    return 2024 - elem
+} );
+
+console.log(korokES5)
+
+// es6
+
+let korokES6 = evek.map(elem => 2024 - elem);
+
+console.log(korokES6)
+
+korokES6 = evek.map((elem, index) => `Kor ${index}: ${2024 - elem}.`);
+
+console.log(korokES6);
+
+korokES6 = evek.map((elem, index) => {
+    const aktEv = new Date().getFullYear();
+    const kor = aktEv - elem;
+    return `Kor ${index}: ${kor},`;
+})
+
+console.log(korokES6);
+*/
+var dobozES5 = {
+    szin: 'zöld',
+    pozicio: 1,
+    KattinsRám: function() {
+        console.log(this.szin);
+
+        document.querySelector('.green').addEventListener('click', function() {
+            var szoveg = 'Én vagyok a(z) ' + this.pozicio + '. doboz és a színem ' + this.szin + '.'
+            alert(szoveg);
+        })
+    }
+}
+
+dobozES5.KattinsRám();
