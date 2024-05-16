6cff7ddae2286f36dad8ba6856205de2e594fa9b
meszaros16 <levjuce2004@gmail.com>
2024-05-16 11:07:15 +0200

    órai javascript

---
+++ Vicsápi Levente/html/java/koltsegvetes-js-projekt 69/js/app.js
@@ -0,0 +1,53 @@
+// let és const
+
+var nev5 = 'Teszt Elek';
+var kor5 = 40;
+
+nev5 = 'Kiss Pista';
+
+// console.log(nev5);
+
+// es5
+function nyelvVizsga5(siker){
+    if (siker){
+        var keresztNev = 'Ödön';
+        var szuletesiEv = 1980;
+        console.log(keresztNev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga')
+    }
+}
+
+// nyelvVizsga5(true);
+
+// es6
+
+const nev6 = 'Teszt Elek';
+let kor6 = 40;
+
+// nev6 = 'Kiss Pista';
+
+// console.log(nev6);
+
+function nyelvVizsga6(siker){
+    if (siker){
+        let keresztNev = 'Ödön';
+        const szuletesiEv = 1980;
+        console.log(keresztNev + '(születési év: ' + szuletesiEv + ') sikeres volt a vizsga')
+    }
+}
+
+// nyelvVizsga6(true);
+
+/*
+let i = 9;
+for (let i = 0; i < 5; i++){
+    console.log(i);
+}
+*/
+// console.log(i);
+
+var i = 9;
+for (var i = 0; i < 5; i++){
+    console.log(i);
+}
+
+console.log(i);
